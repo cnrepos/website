165ef68ed30dc259a3de29e90d6c7b8201240fff
zhangfisher <wxzhang@126.com>
2023-09-08 11:58:04 +0800

    update baseUrl

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "CnRepos",
-  base: '/cnrepos/website',
+  base: '/website',
   description: "用爱开源",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
